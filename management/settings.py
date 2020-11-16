932b41d3735d0f50e0ae4b9c9c11313a6e5b0e4f
Georjay <geonoble988@gmail.com>
2020-11-16 08:09:15 +0000

    Secret Key Deletion
    Secret Key Deletion

--- management/settings.py
+++ management/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '-=+c^midzm@ig9n2a$oesd!vi3nhnj_anp+$!y_&2-d0ncox)_'
 SECRET_KEY = os.environ.get('JSK_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
